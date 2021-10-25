d97f59dcce833319f6ef7e8893f8f9f7c5458768
VladislavKaraulanov <93125510+VladislavKaraulanov@users.noreply.github.com>
2021-10-25 12:41:36 +0300

    Update script.js

--- NumberRush/script.js
+++ NumberRush/script.js
@@ -1,16 +1,16 @@
-const blockOne = new numberRush('one',{
+new numberRush('one',{
     speed: 10,
     steep: 8,
     maxNumber: 1526
 })
 
-const blockTwo = new numberRush('two',{
+new numberRush('two',{
     speed: 10,
     steep: 1,
     maxNumber: 228
 })
 
-const blockThree = new numberRush('three',{
+new numberRush('three',{
     speed: 10,
     steep: 1,
     maxNumber: 150
